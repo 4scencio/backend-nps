690125fe6a61b6ffdded56cf08cf0f272433de44
4scencio <alessandroascencio@protonmail.com>
2021-02-23 09:30:12 -0800

    install typeorm + sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
+import 'reflect-metadata'
 import express from 'express'
+import './database'
 
 const app = express()
+
+app.post('/users', (request, response) => {
+    return response.json({message: 'salve caralho'})
+})
 
 app.listen(3333, () => console.log('Server is Running :)'))
