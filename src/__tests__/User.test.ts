cc1cc01cabf1517f0b2cfdf006bffc0c02b24eee
4scencio <alessandroascencio@protonmail.com>
2021-02-24 17:13:22 -0800

    User test

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,10 +1,29 @@
 import request from 'supertest'
 import { app } from '../app'
 
-describe('users', () => {
-    request(app).post('/users')
-    .send({
-        email: 'user@example.com',
-        name: 'user example'
+import createConnection from '../database'
+
+describe('Users', () => {
+    beforeAll(async () => {
+        const connection = await createConnection()
+        await connection.runMigrations()
+    })
+
+    it('Should be to able to create a new user', async () => {
+        const response = await request(app).post('/users').send({
+            email: 'user@example.com',
+            name: 'user example'
+        })
+
+        expect(response.status).toBe(201)
+    })
+
+    it('Should not be possible to create a user with the same email', async () => {
+        const response = await request(app).post('/users').send({
+            email: 'user@example.com',
+            name: 'user example'
+        })
+
+        expect(response.status).toBe(400)
     })
 })
